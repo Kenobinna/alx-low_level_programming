e7190e328cc881162b44b8e39283a59defbbfac1
Kenobinna <obinnakay92@gmail.com>
2022-09-13 14:11:06 -0700

    task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints alphabet in a lower case.
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+
+	{
+		_putchar(n);
+	}
+		_putchar('\n');
+}
