fa20d7903bc2bec16c4bd7ea1063f77543b1d13d
Kenobinna <obinnakay92@gmail.com>
2022-09-14 14:10:26 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * print_alphabet
+ * print_alphabet - prints alphabet in small letters
 */
 
 void print_alphabet(void)
 {
 	int ch;
+
 	for (ch = 'a'; ch <= 'z'; ++ch)
 	_putchar(ch);
 	_putchar('\n');
