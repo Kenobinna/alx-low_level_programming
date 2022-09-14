f9eabd0fb6e7db803b3bc85d1dc618c34c629061
Kenobinna <obinnakay92@gmail.com>
2022-09-14 05:18:10 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - Entry point without using printf and put
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *sh = "-putchar"
+	char item[] = "-putchar";
+	int i = 0;
 
-	while (*sh)
+	while (item[i] != '\0')
 
 	{
-		_putchar(*sh);
-		sh++
+		_putchar(item[i]);
+		i++
 	}
 	_putchar('\n');
 
