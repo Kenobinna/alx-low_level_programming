cf22a66efaa096366f43265bb54bcef6ecca2a54
Kenobinna <obinnakay92@gmail.com>
2022-10-13 10:57:05 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,9 +14,9 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 
 	if (n == 0)
-		return (0)
+		return (0);
 
-			va_start(valist, n);
+	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
